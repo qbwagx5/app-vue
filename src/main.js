cda43148491bab2c73f81e49aaf90425d3aba5c1
liushuhao <liushuhao2@sina.com>
2018-07-02 21:16:05 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import app from './APP.vue'
+import {Header} from 'mint-ui'
+Vue.component(Header.name, Header)
+import './lib/mui/css/mui.min.css'
+
+
+ var  vm =new Vue({
+     el:'#app',
+     render:c=>c(app)
+
+ })
