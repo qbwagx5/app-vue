a09d331a22e4e4b157ca3fb1ccd07b0d2bfe8e94
liu <liushuhao2@sina.com>
2018-07-10 08:24:08 +0800

    mock 数据的集成

---
+++ src/components/goods/GoodsList.vue
@@ -0,0 +1,135 @@
+<template>
+
+
+<div class="goods-list">
+   <div class="goods-item">
+
+    <img src="http://g.search3.alicdn.com/img/i3/122610140/TB2yN74BwmTBuNjy1XbXXaMrVXa_!!0-saturn_solar.jpg_220x220.jpg" alt="">
+    <h1 class="title">中袖半袖柔美雪纺衫女2018新款大码女装夏装V领宽松洋气百搭上衣</h1>
+    <div class="info">
+      <p class="price">
+           <span class="now">￥2199</span>
+           <span class="old">￥2399</span>
+      </p>
+      <p class="sell">
+        <span>热卖中</span>
+        <span>剩60件</span>
+      </p>
+     
+    </div>
+    </div>
+
+<div class="goods-item">
+
+    <img src="http://g.search3.alicdn.com/img/i3/122610140/TB2yN74BwmTBuNjy1XbXXaMrVXa_!!0-saturn_solar.jpg_220x220.jpg" alt="">
+    <h1 class="title">中袖半袖柔码女装夏装V领宽松洋气百搭上衣</h1>
+    <div class="info">
+      <p class="price">
+           <span class="now">￥2199</span>
+           <span class="old">￥2399</span>
+      </p>
+      <p class="sell">
+        <span>热卖中</span>
+        <span>剩60件</span>
+      </p>
+     
+    </div>
+</div>
+
+  <div class="goods-item">
+    <img src="http://g.search3.alicdn.com/img/i3/122610140/TB2yN74BwmTBuNjy1XbXXaMrVXa_!!0-saturn_solar.jpg_220x220.jpg" alt="">
+    <h1 class="title">中袖半袖柔美雪纺衫女V领宽松洋气百搭上衣</h1>
+    <div class="info">
+      <p class="price">
+           <span class="now">￥2199</span>
+           <span class="old">￥2399</span>
+      </p>
+      <p class="sell">
+        <span>热卖中</span>
+        <span>剩60件</span>
+      </p>
+     
+    </div>
+</div>
+  <!-- <div class="goods-item">
+    <img src="http://g.search3.alicdn.com/img/i3/122610140/TB2yN74BwmTBuNjy1XbXXaMrVXa_!!0-saturn_solar.jpg_220x220.jpg" alt="">
+    <h1 class="title">中袖半袖柔美雪纺衫女2018新款大码女装夏装V领宽松洋气百搭上衣</h1>
+    <div class="info">
+      <p class="price">
+           <span class="now">￥2199</span>
+           <span class="old">￥2399</span>
+      </p>
+      <p class="sell">
+        <span>热卖中</span>
+        <span>剩60件</span>
+      </p>
+     
+    </div>
+</div> -->
+</div>
+
+
+    
+    
+</template>
+
+<script>
+    
+</script>
+  
+
+
+  <style lang="scss" scoped>
+  .goods-list{
+    display: flex;
+    flex-wrap: wrap;
+    padding: 7px;
+    justify-content: space-between;
+    
+    .goods-item{
+      width: 49%;
+      overflow: hidden;
+      border: 1px solid #ccc;
+      box-shadow: 0 0 8px #ccc;
+      margin:4px 0;
+      padding: 2px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      min-height: 293px;
+      .img{
+        width: 100%;
+      }
+  
+
+      .title{
+        font-size: 14px;
+      }
+      .info{
+        background-color: #eee;
+        p{margin: 0px;}
+      .price{
+        .now{
+          color: red;
+          font-weight: bold;
+          font-size: 16px;
+        }
+        .old{
+          text-decoration: line-through;
+          font-size: 12px;
+          margin-left: 10px;
+        }
+      
+       
+       }
+       .sell{
+         display: flex;
+         justify-content: space-between;
+
+       }
+      }
+    }
+  }
+  
+  </style>
+  
--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,20 +1,26 @@
 
+
+
+
 const Mock = require('mockjs');
 // 获取 mock.Random 对象
 const Random = Mock.Random;
 // mock一组数据
-const productData = function() {
-  let Data = [];
-  for (let i = 0; i < 100; i++) {
-    let data = {
-      name: Random.cname(), // Random.cname() 随机生成一个常见的中文姓名
-      date: Random.date()  // Random.date()指示生成的日期字符串的格式,默认为yyyy-MM-dd
-    }
-    Data.push(data)
+const produceData = function (opt) {
+  console.log('opt', opt);
+  let articles = [];
+  for (let i = 0; i < 30; i++) {
+   let newArticleObject = {
+    title: Random.csentence(5, 30), // Random.csentence( min, max )
+    thumbnail_pic_s: Random.dataImage('300x250', 'mock的图片'), // Random.dataImage( size, text ) 生成一段随机的 Base64 图片编码
+    author_name: Random.cname(), // Random.cname() 随机生成一个常见的中文姓名
+    date: Random.date() + ' ' + Random.time() // Random.date()指示生成的日期字符串的格式,默认为yyyy-MM-dd；Random.time() 返回一个随机的时间字符串
+   }
+   articles.push(newArticleObject)
   }
- 
   return {
-    Data: Data
+   data: articles
   }
-}
-Mock.mock('/news/index', 'post', produceNewsData);
+ }
+Mock.mock('/news', post, produceData);
+
